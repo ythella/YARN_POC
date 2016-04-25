5c0d5b9d4505884d6fd7ba775f6274df4f302295
rsaha-nisum-com <rsaha@nisum.com>
2016-04-25 15:15:24 -0700

    [rajib]: added the gradle build and spring bootstrap

---
+++ src/main/java/launch/Application.java
@@ -0,0 +1,31 @@
+package launch;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@EnableAutoConfiguration
+@RequestMapping("/")
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+        System.out.println("Spring Boot app started with Embedded Tomcat");
+  
+    }
+    
+    @RequestMapping(method = GET)
+    public String home() {
+        System.out.println("HELLO !!... App Started");
+        return "WELCOME to EMPLOYEE PROFILES";
+    }
+
+}
